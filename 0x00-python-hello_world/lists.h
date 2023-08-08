e45cefd7ed4e47698893e688401fd86aa3b5eea9
Lelaabk <lolabkari@gmail.com>
2023-08-08 04:33:48 -0700

    lists header

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,24 +1,24 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdlib.H>
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
- * @in: integer
- * @next: points to next node
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: singly linked list node for 
- * Holberton project
+ * Description: singly linked list node structure
+ * 
  */
-typedef struct listint_S
+typedef struct listint_s
 {
-	int in;
-	struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head);
-
+listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
