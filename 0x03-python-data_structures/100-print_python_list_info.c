df6acf00806ef6470af8ecf7fe90a0e4afbaa5c0
Lelaabk <lolabkari@gmail.com>
2023-08-14 05:44:00 -0700

    print python list info

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,11 +14,11 @@ void print_python_list_info(PyObject *p)
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", alloc);
 
-	for (i = 0; i < size; i++)
+	for (x = 0; x < size; x++)
 	{
-		printf("Element %d: ", i);
+		printf("Element %d: ", x);
 
-		obj = PyList_GetItem(p, i);
+		obj = PyList_GetItem(p, x);
 		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
 }
