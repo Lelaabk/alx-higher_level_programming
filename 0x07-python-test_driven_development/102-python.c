c3c7224452668ffa6987062dd0ebfdbffa414306
Lelaabk <lolabkari@gmail.com>
2023-10-04 06:24:18 -0700

    Some Errors Solved

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -9,22 +9,22 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int len;
+	long int length;
 
 	fflush(stdout);
 
 	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->ty_name, "str") != 0)
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
 	{
 		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
-	len = ((PyASCIIObject *)(p))->len;
+	length = ((PyASCIIObject *)(p))->length;
 
 	if (PyUnicode_IS_COMPACT_ASCII(p))
 		printf("  type: compact ascii\n");
 	else
 		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", len);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
