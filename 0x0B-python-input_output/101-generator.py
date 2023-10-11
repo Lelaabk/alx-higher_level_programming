66788f90550793f739fe33a95e4494ad960f50f9
Layla Abkari <99873278+Lelaabk@users.noreply.github.com>
2023-10-11 14:47:14 +0100

    Update 101-generator.py

--- 0x0B-python-input_output/101-generator.py
+++ 0x0B-python-input_output/101-generator.py
@@ -1,15 +1,34 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+"""101-stats.py is a program that reads IP logs from stdin and
+prints metrics every ten lines or until EOF or Ctrl-C."""
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+
+def print_dict_sorted_nonzero(status_codes):
+    """Subroutine to print status codes with nonzero value in
+    numericalorder.
+
+    Args:
+        status_codes (dict): dictionary of status codes and the
+            number of times each one has been returned.
+    """
+    sorted_keys = sorted(status_codes.keys())
+    print('\n'.join(["{:d}: {:d}".format(k, status_codes[k])
+                     for k in sorted_keys if status_codes[k] != 0]))
+
+if __name__ == "__main__":
+    import sys
+
+    try:
+        total = 0
+        status_codes = \
+            {code: 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
+        for n, line in enumerate(sys.stdin, 1):
+            words = line.split()
+            total += int(words[-1])
+            status_codes[int(words[-2])] += 1
+            if n % 10 == 0:
+                print("File size: {:d}".format(total))
+                print_dict_sorted_nonzero(status_codes)
+    finally:
+        print("File size: {:d}".format(total))
+        print_dict_sorted_nonzero(status_codes)
