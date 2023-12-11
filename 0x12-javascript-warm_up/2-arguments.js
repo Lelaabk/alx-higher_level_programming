832ea259d7310d979229aee9cd123cb738189482
Lelaabk <lolabkari@gmail.com>
2023-12-11 12:56:38 -0800

    Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const args = proccess.argv.slice(2);
+if (args.length === 0)
+	console.log('No argumet');
+else if (args.length === 1)
+	console.log('Argument found');
+else
+	console.log('Arguments found');
