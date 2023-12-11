f1c24de1e0853ba5637e407c1c30486d20541500
Lelaabk <lolabkari@gmail.com>
2023-12-11 13:22:18 -0800

    Value of argument

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args.length === 0) {
+if (process.argv.length === 2) {
   console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log(args[0]);
+  console.log('Arguments found');
 }
