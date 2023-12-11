0a1e4fdc29296f7999fea222f83bcef8d1466288
Lelaabk <lolabkari@gmail.com>
2023-12-11 13:38:59 -0800

    To Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-
-const nombre = parseInt(process.argv[2]);
-if (isNaN(nombre)) {
-  console.log('Not a Number');
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-  console.log('My number:', nombre);
+  console.log('My number:', parseInt(process.argv[2]));
 }
