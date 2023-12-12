9450b7065728f1d3d453bb48e155640da55987b4
Lelaabk <lolabkari@gmail.com>
2023-12-12 04:49:44 -0800

    Concatenation

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,33 +1,6 @@
 #!/usr/bin/node
 
-const f = require('fs');
-if (process.argv.length !== 5) {
-  console.error('Usage: ./102-concat.js file1 file2 destFile');
-  process.exit(1);
-}
-
-const [, , file1, file2, destFile] = process.argv;
-
-f.readFile(file1, 'utf8', (err, data1) => {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-
-  f.readFile(file2, 'utf8', (err, data2) => {
-    if (err) {
-      console.error(err);
-      process.exit(1);
-    }
-
-    const concatenatedContent = data1 + data2;
-
-    f.writeFile(destFile, concatenatedContent, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-        process.exit(1);
-      }
-      console.log(`The content of ${file1} and ${file2} has been successfully concatenated and written to ${destFile}`);
-    });
-  });
-});
+const fs = require('fs');
+const fa = fs.readFileSync(process.argv[2]).toString();
+const sa = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fa + sa);
