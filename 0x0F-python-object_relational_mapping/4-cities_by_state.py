d75ff0a71160b987cf11cee806441217b02a5f11
Lelaabk <lolabkari@gmail.com>
2023-12-15 03:10:14 -0800

    Cities by States

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Script that lists all cities from database hbtn_0e_0_usa. """
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    if len(sys.argv[4]) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=db_name, charset="utf8")
+    cs = db.cursor()
+
+    query = """SELECT cities.id, cities.name, states.name
+               FROM cities
+               JOIN states ON cities.states_id = states.id
+               ORDER BY cities.id ASC"""
+    cs.execute(query)
+    rows = cs.fetchall()
+    for row in rows:
+        print(row)
+
+    cs.close()
+    db.close()
