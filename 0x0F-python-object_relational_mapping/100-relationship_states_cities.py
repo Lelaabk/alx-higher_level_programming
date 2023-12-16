5afaac5560e034d398680979bd21e1c5825b102d
Lelaabk <lolabkari@gmail.com>
2023-12-16 07:40:40 -0800

    List of relationship States & Cities

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,17 +3,19 @@
 
 
 import sys
-from relationship_city import City
 from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
                 sys.argv[1], sys.argv[2], sys.argv[3]),
             pool_pre_ping=True)
+
     Base.metadata.create.all(engine)
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
@@ -21,6 +23,7 @@ if __name__ == "__main__":
     city_jdida = City(name='San Francisco')
 
     state_jdida.cities.append(city_jdida)
+
     session.add(state_jdida)
     session.add(city_jdida)
     session.commit()
