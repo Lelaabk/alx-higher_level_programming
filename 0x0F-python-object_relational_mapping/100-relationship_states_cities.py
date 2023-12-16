bb86bd2b9b9aa53f61b0df4ee59b9a35c9157854
Lelaabk <lolabkari@gmail.com>
2023-12-16 07:42:11 -0800

    List of relationship States & Cities

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-""" Creates State "California" with city "San Francisco". """
-
-
+"""
+Creates the State "California" with the City "San Francisco" from a DB
+"""
 import sys
 from relationship_state import Base, State
 from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
-    engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-                sys.argv[1], sys.argv[2], sys.argv[3]),
-            pool_pre_ping=True)
 
-    Base.metadata.create.all(engine)
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state_jdida = State(name='California')
-    city_jdida = City(name='San Francisco')
+    newState = State(name='California')
+    newCity = City(name='San Francisco')
+    newState.cities.append(newCity)
 
-    state_jdida.cities.append(city_jdida)
-
-    session.add(state_jdida)
-    session.add(city_jdida)
+    session.add(newState)
+    session.add(newCity)
     session.commit()
-
-    session.close()
