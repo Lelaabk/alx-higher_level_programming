171feb53f0be5f37c391bb6becfe107b14dc02da
Lelaabk <lolabkari@gmail.com>
2024-01-05 15:48:37 -0800

    Body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # Script to display body of response for 200 status code
 
-curl -sL -w "%{http_code}" "$1" -o /dev/null | {
-	read -r status
-	if [[ $status -eq 200 ]]; then
-		curl -s "$1"
-	fi
-}
+curl -sX GET "$1" -L 200
