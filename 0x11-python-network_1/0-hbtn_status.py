08f50c43edfadda71c7ac7cf3be43c13319abfc6
Lelaabk <lolabkari@gmail.com>
2024-01-06 15:05:56 -0800

    Fix lines

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,11 +7,11 @@ from urllib import request, error
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    
+
     with request.urlopen(url) as response:
-    body = response.read()
-    body_decoded = body.decode('utf-8')
-    print("Body response:")
-    print("    - type: {}".format(type(body)))
-    print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body_decoded))
+        body = response.read()
+        body_decoded = body.decode('utf-8')
+        print("Body response:")
+        print("    - type: {}".format(type(body)))
+        print("    - content: {}".format(body))
+        print("    - utf8 content: {}".format(body_decoded))
