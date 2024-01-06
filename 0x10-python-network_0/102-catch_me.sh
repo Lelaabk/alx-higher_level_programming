82fa58bd544bed832f57798b0471817180c475f2
Lelaabk <lolabkari@gmail.com>
2024-01-05 16:31:04 -0800

    Catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes request to to 0.0.0.0:5000/catch_me causing server to respond with "You got me!"
-curl -s -X PUT -d "user_id=98" -H "Origin:HolbertonSchool" 0.0.0.0:5000/catch_me
+curl -sL -X PUT -H "Origin:HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
