d81bf2aa22db381da3d4fe6c931fd9372b74b29a
Lelaabk <lolabkari@gmail.com>
2024-01-06 14:58:32 -0800

    Fix lines

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,15 @@
 "Fetches https://alx-intranet.hbtn.io/status using urllib package."
 
 
-import urllib.request
+from urllib import request
 
 
 if __name__ == "__main__":
-    url = 'https://alx-intranet.hbtn.io/status'
+    url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         body = response.read()
-        body_decoded = body.decode("utf-8")
+        body_decoded = body.decode('utf-8')
         print("Body response:")
         print("    - type: {}".format(type(body)))
         print("    - content: {}".format(body))
