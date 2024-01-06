ff51d5f59edcc40294e37f9d552030207da30eaa
Lelaabk <lolabkari@gmail.com>
2024-01-06 09:29:57 -0800

    Status hbtn

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ url = 'https://alx-intranet.hbtn.io/status'
 
 with urllib.request.urlopen(url) as response:
     body = response.read()
+    body_decoded = body.decode('utf-8')
     print("Body response:")
     print("    - type: {}".format( type(body)))
     print("    - content: {}".format(body))
-    print("    - utf8 content: {}".format(body.decode('utf8')))
+    print("    - utf8 content: {}".format(body_decoded))
