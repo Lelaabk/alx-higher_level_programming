af9f18e9c842734b9046c46b118d49dfd628d66a
Lelaabk <lolabkari@gmail.com>
2024-01-05 16:36:23 -0800

    Fix lines

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,21 +4,17 @@
 
 def find_peak(list_of_integers):
     "Finds peak in list of unsorted int."
-    if list_of_integers == []:
+    if not list_of_integers:
         return None
 
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
+    low, high = 0, len(list_of_integers) - 1
 
-     mid = int(size / 2)
-     peak = list_of_integers[mid]
+    while low < high:
+        mid = (low + high) // 2
 
-     if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
-        return peak
-     elif peak < list_of_integers[mid - 1]:
-        return find_peak(list_of_integers[:mid])
-    else:
-        return find_peak(list_of_integers[mid + 1:])
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
